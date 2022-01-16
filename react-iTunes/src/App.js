b71bf6f4315b3c521c6ffdbac4225f3fc854af0b
june65 <june0100726@gmail.com>
2022-01-16 22:40:50 +0900

    Building structure-iTunes(#1)

---
+++ react-iTunes/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TopBanner from './TopBanner';
+import MiddlePart from './MiddlePart';
+
+function App() {
+  return (
+    <div>
+      <TopBanner />
+      <div>
+      <MiddlePart />
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
