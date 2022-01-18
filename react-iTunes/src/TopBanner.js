49c34fdfe43ae23bb8905d2d6a0515ef255a0b0a
june65 <june0100726@gmail.com>
2022-01-18 16:17:56 +0900

    Building structure TopBanner.js-iTunes

--- react-iTunes/src/TopBanner.js
+++ react-iTunes/src/TopBanner.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import img_Menu from './image/TopBanner/menu.png';
+import {FaHeart, FaSearch} from 'react-icons/fa';
 
-function TopBanner() {
-  return <div>TopBanner</div>
-}
+function TopBanner({ onChange, searchWord }) {
+  return (
+  <div id="TopBanner_grid">
+
+    <div id="menu">
+      <img src={img_Menu} />
+    </div>
+
+    <div id="title">iTunes-Playground</div>
+
+    <div id="search">
+      <FaSearch/>
+      <input type="text" placeholder="검색"  onChange={onChange} value={searchWord}/>
+    </div>
+
+    <div id="heart"> 
+      <FaHeart/>
+    </div>
+    
+  </div>
+  )
+} 
 
 export default TopBanner;
