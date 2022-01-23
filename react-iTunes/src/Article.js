f79986e267793b152d71ac64a6f5534bb5284004
june65 <june0100726@gmail.com>
2022-01-23 23:13:00 +0900

    Design Article.scss-iTunes(#8)

--- react-iTunes/src/Article.js
+++ react-iTunes/src/Article.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './Article.scss';
 
 function Article({ article }) {
   return (
-    <div>
-      <span>{article.id}</span>
-      <img src={article.image}/>
-      <span>({article.Contents})</span>
-      <span>{article.name}</span>
+    <div className="Article_Wrapper">
+      <div className="Article_Image_Wrapper"><img className='Article_Image' src={article.image}/></div>
+      <div className="Article_Text_Wrapper">
+      <div className="Article_Name">{article.name}</div>
+      <div className="Article_Contents">{article.Contents}</div>
+      </div>
     </div>
   );
 }
